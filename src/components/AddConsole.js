e8ec9ec2b3f77e95a49476f1e7c3716f5b7f8e15
Mateo Gomez-Randulfe <mateorandulfe@gmail.com>
2021-08-14 13:27:34 +0200

    fet: added components

--- src/components/AddConsole.js
+++ src/components/AddConsole.js
@@ -4,10 +4,13 @@ import React, { useState } from 'react';
 
 import CreateAdd from './CreateAdd';
 import Data from './Data';
+import Adds from './Adds';
+import { FaRegEdit, FaRegTrashAlt } from "react-icons/fa";
 
 const AddConsole = ({match: {params:{id}}})=>{
 
     const [show, setShow] = useState(false);
+    const addExample = Adds[0];
 
     let match = Data.find(obj => {
         return obj.productName === id
@@ -25,6 +28,25 @@ const AddConsole = ({match: {params:{id}}})=>{
         <div className='cardContainer'>
             <button className='btn' onClick={() => setShow(!show)}>Create Add</button>
             {createPage}
+
+            <div className='card' style={{cursor:'default', width: '30%'}}>
+            <img className='cardImage' src={addExample.imageUrl}/>
+            <h1 className='cardTitle'>{addExample.headline}</h1>
+            <p className='cardText'>
+                {addExample.description}
+            </p>
+            <div style={{display:'flex', justifyContent:'space-between'}}>
+                <span className='icons'>
+                <FaRegEdit></FaRegEdit>
+                </span>
+            <span className='icons'>
+            <FaRegTrashAlt></FaRegTrashAlt>
+            </span>
+            </div>
+
+        </div>
+
+
         </div>
     )
 }
